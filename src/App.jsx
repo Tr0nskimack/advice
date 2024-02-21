a5f766dff55f25e9e0b710388a527fc3e951528f
unknown <jmarcanoy@gmail.com>
2024-02-21 07:02:17 -0400

    pading top

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className="bg-[#1f2632] h-screen flex justify-center items-center transition-all ">
       {/* card */}
       <div className="bg-[#323a49] relative w-[370px] h-[230px] shadow-xl rounded-lg flex  items-center flex-col">
-        <div className=" mt-4">
+        <div className=" mt-6">
           <h1 className="text-center uppercase tracking-widest font-semibold text-[#52ffa8] text-[8px]">
             advice #117
           </h1>
@@ -13,7 +13,7 @@ const App = () => {
             "It is easy to shit up and take notice, what's dificult is getting
             up and talking action."
           </p>
-          <img src="pattern-divider-desktop.svg" className="px-10 pt-14"  alt="divider" />
+          <img src="pattern-divider-desktop.svg" className="px-10 pt-12"  alt="divider" />
         </div>
         <div className="bg-[#52ffa8] p-3 rounded-full absolute transform -translate-x-[0px] translate-y-[208px] ">
         <img src="icon-dice.svg" className="w-[16px]"  alt="dado" />
